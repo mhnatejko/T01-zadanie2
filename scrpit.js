35ba4e5f4a9f2b9d4824755765f4459e220fe5d4
Maksymilian Hnatejko <maksymilian.hnatejko@gmail.com>
2017-03-08 22:05:45 +0100

    initial commit

---
+++ scrpit.js
@@ -0,0 +1,5 @@
+var a = $(".grid div, .grid-12 div"),
+    b = $(".nav a[href^='http']"),
+    c = $("input [type='radio']:not:checked, input[type='checkbox']:not:checked),
+    d = $("#text div p:first:empty"),
+    e = $(".pagination-item:not('span')");
